684abe84ce8f6b8896ee2341c94954a653cd84e3
p.phillpott <p.phillpott@acfr.usyd.edu.au>
2024-05-29 13:01:11 +1000

    rm dataset

--- train_yolov8.py
+++ train_yolov8.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data='data.yaml', epochs=5, imgsz=640)
+results = model.train(data="data.yaml", epochs=2, imgsz=640)
